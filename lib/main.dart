0c7ef0adaf7732f415d8792d5925cb90187d4d55
dennis weyner <dennisweyner@gmail.com>
2021-01-31 16:14:10 -0400

    implementado Função Remover() item da lista

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,8 @@ class _HomePageState extends State<HomePage> {
   var newTaskCtrl = TextEditingController();
 
   void add() {
+    if (newTaskCtrl.text.isEmpty) return;
+
     setState(() {
       widget.items.add(
         Item(
@@ -45,6 +47,12 @@ class _HomePageState extends State<HomePage> {
         ),
       );
       newTaskCtrl.text = "";
+    });
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
     });
   }
 
@@ -70,15 +78,22 @@ class _HomePageState extends State<HomePage> {
         itemCount: widget.items.length,
         itemBuilder: (BuildContext ctxt, int index) {
           final item = widget.items[index];
-
-          return CheckboxListTile(
-            title: Text(item.title),
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                });
+              },
+            ),
             key: Key(item.title),
-            value: item.done,
-            onChanged: (value) {
-              setState(() {
-                item.done = value;
-              });
+            background: Container(
+              color: Colors.red[900],
+            ),
+            onDismissed: (direction) {
+              remove(index);
             },
           );
         },
